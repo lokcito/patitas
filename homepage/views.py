7bfbbea84f11719f94b70e0f122530f89dd404f1
ray <enciso.leo@gmail.com>
2024-11-19 14:06:55 -0800

    se incluyo pagina homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def v_index(request):
+    context = {}
+    return render(request, "homepage/index.html", 
+                  context)
