a5002fa3a372bf30e134e63c80e411a40b94335c
ray <enciso.leo@gmail.com>
2024-11-19 17:00:28 -0800

    incluidos proveedores

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,39 @@ from django.shortcuts import render
 
 # Create your views here.
 def v_index(request):
-    context = {}
+    context = {
+        "providers": [
+            {
+                "first_name": "Pepito",
+                "last_name": "Rojas",
+                "photo": "https://whoofpoint.com/media/images/card_id/profile_73870520.png"
+            },
+            {
+                "first_name": "Juan",
+                "last_name": "Ramirez"
+            },
+            {
+                "first_name": "Maria",
+                "last_name": "Suarez"
+            },
+            {
+                "first_name": "Vanessa",
+                "last_name": "Suarez"
+            },
+            {
+                "first_name": "Michael",
+                "last_name": "Suarez"
+            },
+            {
+                "first_name": "Ramiro",
+                "last_name": "Suarez"
+            },
+            {
+                "first_name": "Micky",
+                "last_name": "Suarez"
+            },
+        ]
+    }
+
     return render(request, "homepage/index.html", 
                   context)
